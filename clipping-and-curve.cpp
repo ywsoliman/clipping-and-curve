06d4db839b0dfebe0c113cead4ddb311dd8293b5
Youssef W. Soliman <ywsoliman@gmail.com>
2022-05-09 03:01:56 +0200

    Initial commit

---
+++ clipping-and-curve.cpp
@@ -0,0 +1,41 @@
+#include <Windows.h>
+
+LRESULT WINAPI MyWndProc(HWND hwnd, UINT mcode, WPARAM wp, LPARAM lp)
+{
+	HDC hdc = GetDC(hwnd);
+	switch (mcode)
+	{
+	case WM_CLOSE:
+		DestroyWindow(hwnd);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default: return DefWindowProc(hwnd, mcode, wp, lp);
+	}
+	return 0;
+}
+int APIENTRY WinMain(HINSTANCE hinst, HINSTANCE pinst, LPSTR cmd, int nsh)
+{
+	WNDCLASS wc;
+	wc.cbClsExtra = wc.cbWndExtra = 0;
+	wc.hbrBackground = (HBRUSH)GetStockObject(LTGRAY_BRUSH);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hInstance = hinst;
+	wc.lpfnWndProc = MyWndProc;
+	wc.lpszClassName = L"MyClass";
+	wc.lpszMenuName = NULL;
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&wc);
+	HWND hwnd = CreateWindow(L"MyClass", L"My First Window", WS_OVERLAPPEDWINDOW, 0, 0, 800, 600, NULL, NULL, hinst, 0);
+	ShowWindow(hwnd, nsh);
+	UpdateWindow(hwnd);
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return 0;
+}
